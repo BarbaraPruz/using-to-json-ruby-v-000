a44a1b46370dd0e336227e755b1beb16110ee3b7
barbara.prusiewicz prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-21 22:07:21 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -38,14 +38,14 @@ describe 'form' do
 #route not supported
   # it 'shows a new form that submits content and redirects and prints out params' do
   #   @post = Post.create(title: "My Post", description: "My post desc")
-  # 
+  #
   #   visit edit_post_path(@post)
-  # 
+  #
   #   fill_in 'post[title]', with: "My edit"
   #   fill_in 'post[description]', with: "My post description"
-  # 
+  #
   #   click_on "Update Post"
-  # 
+  #
   #   expect(page).to have_content("My edit")
   # end
 end
